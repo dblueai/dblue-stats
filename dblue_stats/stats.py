7d324764d647ae8bd2d21b19e505052a659a495b
Rajesh Hegde <rajesh.p.hegde@gmail.com>
2020-04-16 12:57:26 +0530

    refactor numerical distribution  On branch develop 	modified:   data/statistics.json 	modified:   dblue_stats/stats.py

--- dblue_stats/stats.py
+++ dblue_stats/stats.py
@@ -35,6 +35,12 @@ class DataBaselineStats:
         if not data_type:
             raise DblueDataStatsException("Data type not found: %s" % column.dtype.name)
 
+        distinct_values = column.unique()
+        is_bool = len(set(distinct_values) - {0, 1}) == 0
+
+        if is_bool:
+            data_type = "string"
+
         return data_type
 
     @classmethod
@@ -51,10 +57,43 @@ class DataBaselineStats:
         return _quantiles
 
     @classmethod
+    def get_numerical_distribution(cls, column: pd.Series):
+        # distinct_values = column.unique()
+        # if len(distinct_values) <= 20:
+        #     return {}
+
+        bin_size = 10
+
+        labels = [str(x + 1) for x in range(bin_size)]
+        cuts, bins = pd.cut(x=column, bins=bin_size, precision=2, labels=labels, retbins=True)
+
+        value_counts = cuts.value_counts(normalize=True, sort=False).to_dict()
+
+        distribution = []
+
+        for index, bin_value in enumerate(bins[:-1]):
+            _bin = {
+                "lower_bound": bin_value,
+                "upper_bound": bins[index + 1],
+                "percent": value_counts[str(index + 1)] * 100  # Normalize to 100
+            }
+
+            distribution.append(_bin)
+
+        return distribution
+
+    @classmethod
+    def get_categorical_distribution(cls, column: pd.Series):
+        value_counts = column.value_counts(normalize=True, sort=False).to_dict()
+
+        return value_counts
+
+    @classmethod
     def get_numerical_stats(cls, column: pd.Series):
         describe = column.describe().to_dict()
 
         quantiles = cls.get_quantiles(column=column)
+        distribution = cls.get_numerical_distribution(column=column)
 
         stats = {
             "mean": describe["mean"],
@@ -63,16 +102,20 @@ class DataBaselineStats:
             "min": describe["min"],
             "max": describe["max"],
             "quantiles": quantiles,
+            "distribution": distribution,
         }
 
         return stats
 
     @classmethod
     def get_categorical_stats(cls, column: pd.Series):
-        value_counts = column.value_counts(normalize=True, sort=False).to_dict()
+        value_counts = cls.get_categorical_distribution(column=column)
 
         distinct_count = len(value_counts.keys())
         _top = sorted(value_counts.items(), key=lambda x: x[1], reverse=True)[0][0]
+
+        # Normalize to 100
+        value_counts = {k: v * 100 for k, v in value_counts.items()}
 
         stats = {
             "distinct_count": distinct_count,
