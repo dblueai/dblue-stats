cb0c3cb2cd8b3f2d8d2890b2169590b24c02efd7
Rajesh Hegde <rajesh.p.hegde@gmail.com>
2020-06-05 10:44:58 +0530

    update house price schema/stats  On branch develop 	modified:   data/house-price-dataset.csv 	modified:   data/house-price-schema.json 	modified:   data/house-price-statistics.json 	modified:   main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import json
 from dblue_stats.schema import JSONSchema
 from dblue_stats.stats import DataBaselineStats
 
-data_file_path = 'data/bank-churn-dataset.csv'
-target_column_name = "Exited"
-schema_output_path = "data/bank-churn-schema.json"
-stats_output_path = "data/bank-churn-statistics.json"
+data_file_path = 'data/house-price-dataset.csv'
+target_column_name = "SalePrice"
+schema_output_path = "data/house-price-schema.json"
+stats_output_path = "data/house-price-statistics.json"
 
 
 def main():
@@ -16,6 +16,7 @@ def main():
         target_column_name=target_column_name,
         output_path=schema_output_path
     )
+
     print(json.dumps(schema, indent=4))
 
     # Stats
